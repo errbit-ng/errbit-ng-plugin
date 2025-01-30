fb8e3a030f3c22f0f17b38ef28e3202ac67d6a7d
Igor Zubkov <igor.zubkov@gmail.com>
2025-01-30 23:57:41 +0100

    Rename: errbit_plugin -> errbit-ng-plugin

--- lib/errbit_plugin.rb
+++ lib/errbit_plugin.rb
@@ -1,4 +1,4 @@
-require "errbit_plugin/version"
+# require "errbit_plugin/version"
 require "errbit_plugin/registry"
 require "errbit_plugin/issue_tracker"
 require "errbit_plugin/validate_issue_tracker"
